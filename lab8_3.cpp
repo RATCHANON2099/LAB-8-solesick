4aebdaf3e4532e6e8496bca919b8e17d6a25c283
solesick <solesick@cmu.ac.th>
2022-12-20 17:19:01 +0700

    LAB8_3_COMPLETED

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -2,11 +2,24 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	char ans;
+	if(x=='A'){
+		ans = 'Z';
+	}
+	
+	if(x<65 || x>90){
+		ans = '0';
+	}
+	else{if(x!='A'){
+	
+		ans=x-1;}
+	}
+	return ans;
+
 }
 
 int main(){
-	//Test Case
+	
 	cout << before('A') << "\n";
 	cout << before('B') << "\n";
 	cout << before('P') << "\n";
