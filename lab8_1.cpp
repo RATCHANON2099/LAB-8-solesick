64bde4445ce61ea263b24fca96b286f595c0b11e
solesick <solesick@cmu.ac.th>
2022-12-20 15:20:09 +0700

    LAB8_1_COMPLETED

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 
 int main() {
-    double a  =  20, b = 30, c = 4;
-    double output;
+    double a  =  20, b = 30, c = 4, output;
 
     output = a - (b/c);
     cout << "output: " << output << endl;
